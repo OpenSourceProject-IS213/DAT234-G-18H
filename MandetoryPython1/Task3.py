ab91b5a3aff4e39841b3a86b046b9cc8534ce0ff
root <root@localhost.localdomain>
2018-09-17 21:18:15 -0400

    letters should be an array of letters, but I don't think it is.

--- MandetoryPython1/Task3.py
+++ MandetoryPython1/Task3.py
@@ -2,5 +2,12 @@ password_list = {"chris":"helloworld", "john":"passw1", "nelly":"2hell1", "wendy
 
 
 has_capital_letter = False
-for key in password_list[:1]:
-    if key[0].isdigit():
+#for index, key in enumerate(password_list):
+for key in password_list:
+    password = [password_list[key]]
+    print(password)
+    if password[0].isdigit():
+        for index, letter in enumerate(password, start=1):
+            print("xD")
+            if letter.isupper():
+                print("yay")
